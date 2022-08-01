ed1f7d1b3b5e48c3aafbff0ac703b23fedbc2108
LogouT <r.logout@protonmail.com>
2022-07-31 22:47:10 -0300

    fix cronjob

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
+from sched import scheduler
 import requests
 from mail import mail
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+
 def getYellowIpe():
     url = "https://yellowipe.io/api/vacancies/?method=GET"
     response = requests.get(url=url).json()
 
     for i in range(len(response)):
-      if 'Python' in response[i]['tags']:
-        idVacancie = response[i]['idVacancie']
-        title = response[i]['title']
-        positionDescription = response[i]['positionDescription']
-        location = response[i]['location']
-        workplacePolicy = response[i]['workplacePolicy']
-        tags = response[i]['tags']
-        url = f'https://yellowipe.io/vacancies/{idVacancie}'
-        f = open('database.txt', 'r+')
-        if idVacancie in f.read():
-          pass
-        else:
-          mail(idVacancie, title, positionDescription, location, workplacePolicy, tags, url)
-          f.write(f'{idVacancie}\n')
+        if 'Python' in response[i]['tags']:
+            idVacancie = response[i]['idVacancie']
+            title = response[i]['title']
+            positionDescription = response[i]['positionDescription']
+            location = response[i]['location']
+            workplacePolicy = response[i]['workplacePolicy']
+            tags = response[i]['tags']
+            url = f'https://yellowipe.io/vacancies/{idVacancie}'
+            f = open('database.txt', 'r+')
+            if idVacancie in f.read():
+                pass
+            else:
+                mail(idVacancie, title, positionDescription,
+                     location, workplacePolicy, tags, url)
+                f.write(f'{idVacancie}\n')
+
+
+sched.add_job(getYellowIpe, "interval", seconds=30)
+
+sched.start()
--- mail.py
+++ mail.py
@@ -3,27 +3,27 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
 
-""" 
-Local Test Only
+""" Local Test Only
 from dotenv import load_dotenv
 load_dotenv()
 email = os.getenv('EMAIL')
-password = os.getenv('PASSWORD') 
-"""
+password = os.getenv('PASSWORD')  """
+
 
 email = os.environ['EMAIL']
 password = os.environ['PASSWORD']
 
+
 def mail(idVacancie, title, positionDescription, location, workplacePolicy, tags, url):
-  mail_body = f'ID: {idVacancie}\nTítulo: {title}\n\nDescrição: {positionDescription}\n\nLocalização: {location}\nPolítica: {workplacePolicy}\nTags: {tags}\n\nUrl: {url}'
-  message = MIMEMultipart()
-  message['From'] = email
-  message['To'] = email
-  message['Subject'] = f'Nova vaga YellowIpe: {title}'
-  message.attach(MIMEText(mail_body,'plain'))
-  session = smtplib.SMTP('smtp.gmail.com', 587)
-  session.starttls()
-  session.login(email, password)
-  text = message.as_string()
-  session.sendmail(email, email, text)
-  session.quit()
+    mail_body = f'ID: {idVacancie}\nTítulo: {title}\n\nDescrição: {positionDescription}\n\nLocalização: {location}\nPolítica: {workplacePolicy}\nTags: {tags}\n\nUrl: {url}'
+    message = MIMEMultipart()
+    message['From'] = email
+    message['To'] = email
+    message['Subject'] = f'Nova vaga YellowIpe: {title}'
+    message.attach(MIMEText(mail_body, 'plain'))
+    session = smtplib.SMTP('smtp.gmail.com', 587)
+    session.starttls()
+    session.login(email, password)
+    text = message.as_string()
+    session.sendmail(email, email, text)
+    session.quit()
