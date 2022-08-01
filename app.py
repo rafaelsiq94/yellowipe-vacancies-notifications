d3344802893948f5223b3df67a47d9bf2968fba0
LogouT <r.logout@protonmail.com>
2022-07-31 22:26:22 -0300

    first test

---
+++ app.py
@@ -0,0 +1,26 @@
+import requests
+from mail import mail
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=60)
+def getYellowIpe():
+    url = "https://yellowipe.io/api/vacancies/?method=GET"
+    response = requests.get(url=url).json()
+
+    for i in range(len(response)):
+      if 'Python' in response[i]['tags']:
+        idVacancie = response[i]['idVacancie']
+        title = response[i]['title']
+        positionDescription = response[i]['positionDescription']
+        location = response[i]['location']
+        workplacePolicy = response[i]['workplacePolicy']
+        tags = response[i]['tags']
+        url = f'https://yellowipe.io/vacancies/{idVacancie}'
+        f = open('database.txt', 'r+')
+        if idVacancie in f.read():
+          pass
+        else:
+          mail(idVacancie, title, positionDescription, location, workplacePolicy, tags, url)
+          f.write(f'{idVacancie}\n')
